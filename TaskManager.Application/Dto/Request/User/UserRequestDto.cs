92dee4d00952af98e5bcc7c33f11ccf446fc154d
José Augusto Scherer <jose.augusto.github@gmail.com>
2026-05-20 10:05:45 -0300

    - User request/response Dto

---
+++ TaskManager.Application/Dto/Request/User/UserRequestDto.cs
@@ -0,0 +1,3 @@
+namespace TaskManager.Application.Dto.Request.User;
+
+public record UserRequestDto(string Name, string Email, string Password);
---
+++ TaskManager.Application/Dto/Response/User/UserResponseDto.cs
@@ -0,0 +1,3 @@
+namespace TaskManager.Application.Dto.Response.User;
+
+public record UserResponseDto(Guid Id, string Name,string Email);
