940fb40ae0437d03d2c82746cb3be294755111df
José Augusto Scherer <jose.augusto.github@gmail.com>
2026-05-20 10:55:14 -0300

    - User request validation

---
+++ TaskManager.Application/Validators/User/UserRequestDtoValidation.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using TaskManager.Application.Dto.Request.User;
+
+namespace TaskManager.Application.Validators.User;
+
+internal sealed class UserRequestDtoValidation : AbstractValidator<UserRequestDto>
+{
+    public UserRequestDtoValidation()
+    {
+        RuleFor(x => x.Name)
+            .NotNull().NotEmpty().WithMessage("Name is required");
+        
+        RuleFor(x => x.Email)
+            .NotNull().NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid Email format");
+
+        RuleFor(x => x.Password)
+            .NotNull().NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+    }
+}
