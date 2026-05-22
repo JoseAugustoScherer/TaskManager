79da22d0c2befd99f4f198a8bc57ec787aed9e2d
José Augusto Scherer <jose.augusto.github@gmail.com>
2026-05-22 17:07:56 -0300

    - Migration

--- TaskManager.Infrastructure/Data/Migrations/20260522200549_User.Designer.cs
+++ TaskManager.Infrastructure/Data/Migrations/20260522200549_User.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskManager.Infrastructure.Data;
 
 #nullable disable
 
-namespace TaskManager.Infrastructure.Data.Migrations
+namespace TaskManager.Infrastructure.Migrations
 {
     [DbContext(typeof(TaskManagerDbContext))]
-    partial class TaskManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260522200549_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
